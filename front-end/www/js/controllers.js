e191ee408f53959fad9577c878c15c9327028996
Dorian BLANC <dorian.blanc@atos.net>
2017-01-12 16:48:35 +0100

    test interface

--- front-end/www/js/controllers.js
+++ front-end/www/js/controllers.js
@@ -6,4 +6,41 @@ angular.module('starter.controllers', [])
 
 .controller('StartCtrl', function($scope) {
 
+    $scope.$on("$ionicView.enter", function(event, data){
+        initMap();
+    });
+
+    function initMap() {
+        var map = new google.maps.Map(document.getElementById('map'), {
+            center: {lat: 43.6221174, lng: 7.0391009},
+            zoom: 13
+        });
+        var infoWindow = new google.maps.InfoWindow({map: map});
+
+        // Try HTML5 geolocation.
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(function(position) {
+                var pos = {
+                    lat: position.coords.latitude,
+                    lng: position.coords.longitude
+                };
+
+                infoWindow.setPosition(pos);
+                infoWindow.setContent('Location found.');
+                map.setCenter(pos);
+            }, function() {
+                handleLocationError(true, infoWindow, map.getCenter());
+            });
+        } else {
+            // Browser doesn't support Geolocation
+            handleLocationError(false, infoWindow, map.getCenter());
+        }
+    }
+
+    function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+        infoWindow.setPosition(pos);
+        infoWindow.setContent(browserHasGeolocation ?'Error: The Geolocation service failed.' : 'Error: Your browser doesn\'t support geolocation.');
+    }
+
+
 });
