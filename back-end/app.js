e5f0ff16189f89bf86fcd989c0d25e80b7119301
Dorian BLANC <dorian.blanc@atos.net>
2017-01-12 14:18:38 +0100

    Première version d'un back-end qui permet de tester la base MongoDB au travers de moongoose

---
+++ back-end/app.js
@@ -0,0 +1,25 @@
+// Load mongoose package
+var mongoose = require('mongoose');
+// Connect to MongoDB and create/use database called todoAppTest
+mongoose.connect('mongodb://localhost/impleQuestionTest');
+// Create a schema
+var SimpleQuestionSchema = new mongoose.Schema({
+  name: String,
+  question: String,
+  answer: String,
+  nb_point: Number,
+  updated_at: { type: Date, default: Date.now },
+});
+// Create a model based on the schema
+var SimpleQuestion = mongoose.model('SimpleQuestion', SimpleQuestionSchema);
+
+
+// Create a question in memory
+var question = new SimpleQuestion({name: "Première Question", question: "Quel est la taille du zizi de manuel", answer: "10 centimètres", nb_point = 10});
+// Save it to database
+question.save(function(err){
+  if(err)
+    console.log(err);
+  else
+    console.log(question);
+});
