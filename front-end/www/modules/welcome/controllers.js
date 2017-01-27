51de4ad1f0b0761c64ead5b2ba9ca106bd1b1382
Dorian BLANC <dorian.blanc@atos.net>
2017-01-27 14:51:51 +0100

    styling welcome page

--- front-end/www/modules/welcome/controllers.js
+++ front-end/www/modules/welcome/controllers.js
@@ -2,80 +2,82 @@ angular.module('Welcome')
 
 .controller('WelcomeCtrl', function($scope, GameService, $rootScope, $interval) {
 
-  $scope.$on("$ionicView.enter", function(event, data){
-    GameService.getGames($scope).then(function(response) {
-      var games = response.data;
+    $scope.$on("$ionicView.enter", function(event, data){
+        GameService.getGames($scope).then(function(response) {
+            var games = response.data;
 
-      //If the game is started we start the count down of play time remaining
-      angular.forEach(games, function(game) {
-        startCountDown(game, $interval);
-      });
+            //If the game is started we start the count down of play time remaining
+            angular.forEach(games, function(game) {
+                startCountDown(game, $interval);
+            });
 
-      $scope.games = games;
+            $scope.games = games;
+        });
     });
-  });
 })
 
 .controller('SignInCtrl', function($scope, $rootScope, $state, $ionicHistory, $ionicPopup, UserService) {
 
-  $scope.$on("$ionicView.enter", function(event, data){
-    if (typeof(Storage) !== "undefined" && localStorage.getItem("userId") !== null) {
-      UserService.getUser(localStorage.getItem("userId")).then(function(response) {
-        var user = response.data;
-        delete user.password;
-        $rootScope.loggedInUser = user;
+    $scope.$on("$ionicView.enter", function(event, data){
+        initSkewTitle();
+        
+        if (typeof(Storage) !== "undefined" && localStorage.getItem("userId") !== null) {
+            UserService.getUser(localStorage.getItem("userId")).then(function(response) {
+                var user = response.data;
+                delete user.password;
+                $rootScope.loggedInUser = user;
 
-        $ionicHistory.nextViewOptions({
-          disableBack: true,
-          disableAnimate: true
-        });
+                $ionicHistory.nextViewOptions({
+                    disableBack: true,
+                    disableAnimate: true
+                });
 
-        $state.go('app.welcome');
-      });
-    }
-
-    $scope.user = {};
-  });
-
-  $scope.signIn = function (user) {
-    UserService.getUserByEmail(user.username).then(function(response) {
-      var user = response.data;
-      if (user == null) {
-        $ionicPopup.alert({
-          title: "Username or password invalid",
-          template: "Please check your credentials"
-        });
-      }else {
-        delete user.password;
-        $rootScope.loggedInUser = user;
-
-        if (typeof(Storage) !== "undefined") {
-          localStorage.setItem("userId", $rootScope.loggedInUser._id);
+                $state.go('app.welcome');
+            });
         }
 
-        $ionicHistory.nextViewOptions({
-          disableBack: true
-        });
-
-        $state.go('app.welcome');
-      }
+        $scope.user = {};
     });
-  };
+
+    $scope.signIn = function (user) {
+        UserService.getUserByEmail(user.username).then(function(response) {
+            var user = response.data;
+            if (user == null) {
+                $ionicPopup.alert({
+                    title: "Username or password invalid",
+                    template: "Please check your credentials"
+                });
+            }else {
+                delete user.password;
+                $rootScope.loggedInUser = user;
+
+                if (typeof(Storage) !== "undefined") {
+                    localStorage.setItem("userId", $rootScope.loggedInUser._id);
+                }
+
+                $ionicHistory.nextViewOptions({
+                    disableBack: true
+                });
+
+                $state.go('app.welcome');
+            }
+        });
+    };
 
 })
 
 .controller('SignUpCtrl', function($scope) {
 
-  $scope.$on("$ionicView.enter", function(event, data){
+    $scope.$on("$ionicView.enter", function(event, data){
 
-  });
+    });
 
 })
 
 .controller('ForgotPasswordCtrl', function($scope) {
 
-  $scope.$on("$ionicView.enter", function(event, data){
+    $scope.$on("$ionicView.enter", function(event, data){
 
-  });
+    });
 
 });
