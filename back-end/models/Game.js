633d03e8482b6bbc17f2ba6b161501f2f9373897
Dorian Blanc <blanc.dorian.pro@gmail.com>
2017-02-11 21:53:58 +0100

    maj backend

--- back-end/models/Game.js
+++ back-end/models/Game.js
@@ -1,13 +1,7 @@
-var ScoreBoardSchema = new mongoose.Schema({
-  user: { type : mongoose.Schema.Types.ObjectId, ref: 'User', required : true},
-  score: { type : Number, default: 0 },
-  questionsAnswered: [String]
-},{ _id : false });
-
 var GameSchema = new mongoose.Schema({
     name: { type : String, required : true, index: {unique: true} },
     questions: [String],
-    scoreBoard: [ScoreBoardSchema],
+    scoreBoard: [{ type : mongoose.Schema.Types.ObjectId, ref: 'ScoreBoard'}],
     duration: { type : Number, default: 90 },
     playerNb: { type : Number, default: 5 },
     status: { type : String, default : 'NOT_STARTED' },
--- back-end/routes/game.js
+++ back-end/routes/game.js
@@ -1,12 +1,15 @@
 var express = require('express');
 var router = express.Router();
 
+var ScoreBoard = require('../models/ScoreBoard.js');
+var User = require('../models/User.js');
 var Game = require('../models/Game.js');
 
 /* GET /games listing. */
 router.get('/', function(req, res, next) {
   Game.find()
-  .populate('scoreBoard.user')
+  .populate('scoreBoard')
+  .populate({ path: 'scoreBoard.user', model : User })
   .exec(function (err, game) {
     if (err) return next(err);
     res.json(game);
@@ -24,7 +27,8 @@ router.post('/', function(req, res, next) {
 /* GET /game/id */
 router.get('/:id', function(req, res, next) {
   Game.findById(req.params.id)
-  .populate('scoreBoard.user')
+  .populate('scoreBoard')
+  .populate({ path: 'scoreBoard.user', model : User })
   .exec(function (err, game) {
     if (err) return next(err);
     res.json(game);
@@ -42,6 +46,23 @@ router.get('/user/:id', function(req, res, next) {
 /* PUT /user/:id */
 router.put('/:id', function(req, res, next) {
   Game.findByIdAndUpdate(req.params.id, req.body, {new: true}, function (err, post) {
+    if (err) return next(err);
+    res.json(post);
+  });
+});
+
+router.put('/:gameId/scoreBoardEntry/:scoreBoardEntryId', function(req, res, next) {
+  Game.findById(req.params.gameId)
+  .populate({
+    path: 'scoreBoard',
+    populate: { path: 'user' }
+  })
+  .exec(function (err, game) {
+    if (err) return next(err);
+    console.log(game);
+  });
+
+  ScoreBoard.findByIdAndUpdate(req.params.scoreBoardEntryId, req.body, {new: true}, function (err, post) {
     if (err) return next(err);
     res.json(post);
   });
