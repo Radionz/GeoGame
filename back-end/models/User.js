810d7db47e072e6813f47e21846f7e90333bc967
Dorian Blanc <blanc.dorian.pro@gmail.com>
2017-02-12 15:46:52 +0100

    team color

--- back-end/models/User.js
+++ back-end/models/User.js
@@ -1,5 +1,6 @@
 var UserSchema = new mongoose.Schema({
   name:  { type : String, required : true },
+  team:  { type : String, enum: ['blue', 'red', 'pink', 'yellow', 'green'], required : true, default: 'blue'},
   email: { type : String, required : true, index: {unique: true} },
   password: { type : String, required : true },
   role: { type : String, required : true,  enum: ['Player', 'Admin'], default: 'Player'},
